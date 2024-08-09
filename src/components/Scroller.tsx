fdf265e53f300bcf3f249a095c1bbb4f3b944d04
pbuieifu <pbui@eifu.com>
2024-08-09 15:35:08 -0400

    better scrolling

--- src/components/Scroller.tsx
+++ src/components/Scroller.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "../assets/css/Scroller.css";
 import TickerContainer from "./TickerContainer";
 
@@ -9,13 +9,34 @@ interface ScrollerProps {
 
 const Scroller: React.FC<ScrollerProps> = ({ children, sectionNames }) => {
   const [isExpanded, setIsExpanded] = useState(window.innerWidth >= 769);
+  const scrollerRef = useRef<HTMLDivElement>(null);
 
   const toggleButtonContainer = () => {
     setIsExpanded(!isExpanded);
   };
 
+  const handleScrollToSection = (index: number) => {
+    const section = document.getElementById(`section-${index}`);
+    const scroller = scrollerRef.current;
+
+    if (section && scroller) {
+      // Get the top position of the section relative to the scroller-content div
+      const sectionTop = section.offsetTop;
+
+      // Calculate the position to scroll to so that the section is centered in the scroller-content div
+      const offsetPosition = sectionTop - 50;
+
+      // Smoothly scroll to the calculated position within the scroller-content div
+      scroller.scrollTo({ top: offsetPosition, behavior: "smooth" });
+    }
+
+    if (!(window.innerWidth >= 769)) {
+      setIsExpanded(false);
+    }
+  };
+
   return (
-    <div className="scroller">
+    <div className="scroller" ref={scrollerRef}>
       <TickerContainer text="Wonderland 9/7 | Stamina 9/12 | Select Markets 4/12 |&nbsp;" />
       <div
         className={`button-container ${isExpanded ? "expanded" : "collapsed"}`}
@@ -31,18 +52,7 @@ const Scroller: React.FC<ScrollerProps> = ({ children, sectionNames }) => {
         {isExpanded && (
           <div className="section-buttons">
             {children.map((child, index) => (
-              <button
-                key={index}
-                onClick={() => {
-                  document
-                    .getElementById(`section-${index}`)
-                    ?.scrollIntoView({ behavior: "smooth" });
-
-                  if (!(window.innerWidth >= 769)) {
-                    setIsExpanded(false);
-                  }
-                }}
-              >
+              <button key={index} onClick={() => handleScrollToSection(index)}>
                 {sectionNames[index]} {/* Display section name */}
               </button>
             ))}
