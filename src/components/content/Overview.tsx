9aaa04c7b1861f7654f88734c4e8b0873bd4bffd
pbuieifu <pbui@eifu.com>
2024-08-16 15:58:46 -0400

    redoing overview css

--- src/components/content/Overview.tsx
+++ src/components/content/Overview.tsx
@@ -5,30 +5,45 @@ export const Overview = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const slides = [
-    <div className="slide">
-      <img src="/assets/images/jazz.jpg" alt="Jazz Image" />
-    </div>,
-    <div className="slide">
-      <iframe
-        width="100%"
-        height="315"
-        src="https://www.youtube.com/embed/sTfz5AxwBds"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    </div>,
-    <div className="slide">
-      <iframe
-        width="100%"
-        height="300"
-        scrolling="no"
-        frameBorder="no"
-        allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/123456789&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-      ></iframe>
-    </div>,
+    {
+      title: "Jazz Vibes",
+      content: (
+        <div className="slide">
+          <img src="/assets/images/jazz.jpg" alt="Jazz Image" />
+        </div>
+      ),
+    },
+    {
+      title: "Live Performance",
+      content: (
+        <div className="slide">
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/sTfz5AxwBds"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      ),
+    },
+    {
+      title: "SoundCloud Track",
+      content: (
+        <div className="slide">
+          <iframe
+            width="100%"
+            height="300"
+            scrolling="no"
+            frameBorder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/123456789&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          ></iframe>
+        </div>
+      ),
+    },
     // Add more slides as needed
   ];
 
@@ -42,17 +57,20 @@ export const Overview = () => {
 
   return (
     <div className="overview-container">
-      <div className="overview-title">
-        <h1>Selling the Aesthetic of Underground Freedom</h1>
-      </div>
-      <div className="slider-container">
-        <div className="slider">{slides[currentSlide]}</div>
-        <button className="slider-button prev" onClick={handlePrev}>
-          &#10094;
-        </button>
-        <button className="slider-button next" onClick={handleNext}>
-          &#10095;
-        </button>
+      <div className="left-side">
+        <div className="overview-title">
+          <h1>Underground Event Consultancy</h1>
+        </div>
+        <div className="slider-container">
+          <h3 className="slide-title">{slides[currentSlide].title}</h3>
+          <div className="slider">{slides[currentSlide].content}</div>
+          <button className="slider-button prev" onClick={handlePrev}>
+            &#10094;
+          </button>
+          <button className="slider-button next" onClick={handleNext}>
+            &#10095;
+          </button>
+        </div>
       </div>
       <div className="overview-content">
         <p>
